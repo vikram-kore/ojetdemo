3e605066628c313432edab2660b7b01206f68f5e
VIKORE <VIKORE@VIKORE-7400.in.oracle.com>
2021-02-19 14:55:01 +0530

    dashboard list filter added

--- src/js/viewModels/dashboard.js
+++ src/js/viewModels/dashboard.js
@@ -1,12 +1,14 @@
-define(['ojs/ojcore', 'knockout', 'jquery', 'appController', 'ojs/ojmodule-element-utils', 'ojs/ojcontext', 'ojs/ojbootstrap', 'ojs/ojanimation', 'ojs/ojinputtext', 'ojs/ojpopup', 'ojs/ojbutton', 'ojs/ojlistview', 'ojs/ojcollectiontabledatasource', "ojs/ojinputsearch", "ojs/ojcheckboxset"],
- function(oj, ko, $, app, moduleUtils, Context, ojbootstrap_1, AnimationUtils, ArrayDataProvider, ListDataProviderView) {
+define(['ojs/ojcore', 'knockout', 'jquery', 'appController', 'ojs/ojmodule-element-utils', 'ojs/ojcontext', 'ojs/ojbootstrap', 'ojs/ojanimation', 'ojs/ojmodel', 'ojs/ojcollectiondataprovider', 'ojs/ojinputtext', 'ojs/ojpopup', 'ojs/ojbutton', 'ojs/ojlistview', "ojs/ojinputsearch", "ojs/ojcheckboxset"],
+ function(oj, ko, $, app, moduleUtils, Context, ojbootstrap_1, AnimationUtils, Model, CollectionDataProvider) {
     function DashboardViewModel() {
       var self = this;
 
-      // header 
+      self.filtertxt = ko.observable("");
+
       var resolve = Context.getPageContext().getBusyContext().addBusyState({description: "wait for header"});
 
       self.headerConfig = ko.observable({'view':[], 'viewModel':null});
+
       moduleUtils.createView({'viewPath':'views/header.html'}).then(function(view) {
         self.headerConfig({'view':view, 'viewModel': app.getHeaderModel()})
         resolve();
@@ -22,10 +24,82 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'appController', 'ojs/ojmodule-eleme
 
       self.transitionCompleted = function() {
 
+      };    
+    
+
+      self.colorOptions = [
+          { id: "ACTIVE", value: "ACTIVE" },
+          { id: "INACTIVE", value: "INACTIVE" },
+          { id: "CANDIDATE", value: "CANDIDATE" }
+      ];
+
+      self.SalesProfileStatuscheckboxfilter = ko.observableArray([]);
+
+      self.serviceURL = 'http://demo6785834.mockable.io/accounts'; 
+      
+
+      function parseItem(response) {
+        return {
+          SyncLocalId: response['SyncLocalId'],
+            PartyNumber: response['PartyNumber'],
+            OrganizationName:response['OrganizationName'],
+            SalesProfileStatus:response['SalesProfileStatus'],
+            OrganizationDEO_Status_c:response['OrganizationDEO_Status_c'],
+            NextVisit:response['NextVisit'],
+            LastVisit:response['LastVisit']
+        }
+      }
+
+      var Accountmodal = Model.Model.extend({
+        urlRoot: self.serviceURL,
+        parse: parseItem,
+        idAttribute: 'SyncLocalId'
+      });
+
+      var AccountsCollection = new Model.Collection.extend({
+        url: self.serviceURL,
+        model: Accountmodal,
+        comparator: 'SyncLocalId'
+      });
+
+      self.listaccountscollection = new AccountsCollection();
+
+      self.datasource = ko.observableArray();
+
+      self.datasource(new CollectionDataProvider(self.listaccountscollection));
+
+      
+      this.handleValueChanged = (event) => {
+        self.filtertxt(event.detail.value)
+        if (event.detail.value === null || event.detail.value === undefined || event.detail.value === "") {
+          this.datasource(new CollectionDataProvider(this.listaccountscollection));
+        };
+        if (this.filteredCollection === undefined) {
+          this.filteredCollection = this.listaccountscollection.clone();
+          this.filteredDataProvider = new CollectionDataProvider(this.filteredCollection);
+        }
+        var ret = this.listaccountscollection.where({
+          OrganizationName: {
+            value: event.detail.value,
+            comparator: (model, attr, value) => {
+              let OrganizationName = model.get("OrganizationName");
+              if(self.SalesProfileStatuscheckboxfilter().length>0){
+                  if(self.SalesProfileStatuscheckboxfilter().includes(model.get("SalesProfileStatus").trim())==true){
+                    return OrganizationName.toLowerCase().includes(value.toLowerCase());
+                  }else{
+                    return false;
+                  }
+              }else{
+                  return OrganizationName.toLowerCase().includes(value.toLowerCase());
+              }
+            },
+          },
+        });
+        this.filteredCollection.reset(ret);
+        this.datasource(this.filteredDataProvider);
       };
 
-
-      //filter popup
+       //filter popup
       self.startAnimationListener = (event) => {
           let ui = event.detail;
           if (event.target.id !== "popup1") {
@@ -48,9 +122,11 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'appController', 'ojs/ojmodule-eleme
       };
 
       self.cancelListener = function() {
+          document.getElementById('search1').focus();
+          document.getElementById('search1').value= "a";
+          document.getElementById('search1').value= "";
           let popup = document.getElementById("popup1");
           popup.close();
-          console.log();
       };
 
       self.gotoaddaccountlink = function() {
@@ -58,122 +134,7 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'appController', 'ojs/ojmodule-eleme
           url = url.split('?')[0];
           window.location.href = url+"?ojr=addaccount";
       };
-      
-      this.filter = ko.observable("");
-      this.filterChanged = (event) => {
-          const filter = event.detail.rawValue;
-          const filteredCollection = this.DeptCol();
-          if (this.originalDeptCol == undefined && filter !== undefined) {
-              this.originalDeptCol = filteredCollection.clone();
-          }
 
-         /* while (!filteredCollection.isEmpty()) {
-              filteredCollection.pop();
-          }*/
-
-
-        $.getJSON("http://demo6785834.mockable.io/accounts",
-          function (data) {
-            for(let i=0;i<data.length;i++){
-              console.log(i);
-              console.log(self.SalesProfileStatuscheckboxfilter().includes(data[i].SalesProfileStatus));
-              if(self.SalesProfileStatuscheckboxfilter().includes(data[i].SalesProfileStatus)==true){
-
-                data.splice(i);
-              }
-            }
-            self.datasource(new oj.CollectionTableDataSource(self.DeptCol()));
-            
-        });
-          
-      };
-      this.nameFilter = (model, attr, value) => {
-          const deptName = model.get("OrganizationName");
-          return deptName.toLowerCase().indexOf(value.toLowerCase()) > -1;
-      };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-      this.colorOptions = [
-          { id: "ACTIVE", value: "ACTIVE" },
-          { id: "INACTIVE", value: "INACTIVE" },
-          { id: "CANDIDATE", value: "CANDIDATE" }
-      ];
-
-      this.SalesProfileStatuscheckboxfilter = ko.observableArray([]);
-
-
-
-
-
-
-
-
-
-
-      self.serviceURL = 'http://demo6785834.mockable.io/accounts';      
-      self.recordcount = ko.observable(0);
-      self.DeptCol = ko.observable();
-      self.datasource = ko.observable();
-
-      self.parseSaveDept = function (response) {
-        return {
-            SyncLocalId: response['SyncLocalId'],
-            PartyNumber: response['PartyNumber'],
-            OrganizationName:response['OrganizationName'],
-            SalesProfileStatus:response['SalesProfileStatus'],
-            OrganizationDEO_Status_c:response['OrganizationDEO_Status_c'],
-            NextVisit:response['NextVisit'],
-            LastVisit:response['LastVisit']
-
-          };
-      };
-
-      self.parseDept = function(response) {
-        self.recordcount(self.recordcount() + 1);
-        return {SyncLocalId: response['SyncLocalId'],
-            PartyNumber: response['PartyNumber'],
-            OrganizationName:response['OrganizationName'],
-            SalesProfileStatus:response['SalesProfileStatus'],
-            OrganizationDEO_Status_c:response['OrganizationDEO_Status_c'],
-            NextVisit:response['NextVisit'],
-            LastVisit:response['LastVisit']};
-
-      };
-      self.Department = oj.Model.extend({
-        urlRoot: self.serviceURL,
-        parse: self.parseDept,
-        parseSave: self.parseSaveDept,
-        idAttribute: 'SyncLocalId'
-      });
-
-      self.myDept = new self.Department();
-      self.DeptCollection = oj.Collection.extend({
-        url: self.serviceURL,
-        model: self.myDept,
-        comparator: "SyncLocalId"
-      });
-
-      self.DeptCol(new self.DeptCollection());
-      
-      $.getJSON("http://demo6785834.mockable.io/accounts",
-        function (data) {
-          self.datasource(new oj.CollectionTableDataSource(self.DeptCol()));
-          
-      });
     }
     return DashboardViewModel;
   }
